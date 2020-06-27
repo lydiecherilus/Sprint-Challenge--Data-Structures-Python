067d863ac95aae42dc0988ee38fc46d27e7d93c9
lydiecherilus <lydiecherilus@yahoo.fr>
2020-06-26 21:52:32 -0400

    MVP and some stretch goal

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity 
+        self.first_node = None
+        self.second_node = None
+        self.data = []
+        
     def append(self, item):
-        pass
+        if len(self.data) == 0:
+            self.first_node = 0
+            return self.data.append(item)
+
+        elif len(self.data) + 1 <= self.capacity:
+            self.second_node = 1
+            return self.data.append(item)
+
+        else:
+            self.data[self.first_node] = item
+            self.first_node += 1
+            if self.first_node + 1 > self.capacity:
+                self.first_node = 0
 
     def get(self):
-        pass
+        return self.data
